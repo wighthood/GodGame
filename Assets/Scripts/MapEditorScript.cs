34e0f628532e1869552575500f508149d8503d82
wighthood <60099411+wighthood@users.noreply.github.com>
2025-10-02 17:31:52 +0200

    almost got map editing

--- Assets/Scripts/MapEditorScript.cs
+++ Assets/Scripts/MapEditorScript.cs
@@ -1,28 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 
 public class MapEditorScript : MonoBehaviour
 {
     [SerializeField] private Tilemap tilemap;
-    [SerializeField] private Tile[] tiles;
+    [SerializeField] private TileBase[] tiles;
     [SerializeField] private Transform selectionBar;
     [SerializeField] private GameObject buttonPrefab;
     
+    private TileBase _selectedTile;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        foreach (Tile tile in tiles)
+        foreach (TileBase tile in tiles)
         {
             GameObject newButton = Instantiate(buttonPrefab,selectionBar);
             Image buttonImage = newButton.GetComponent<Image>();
-            buttonImage.sprite = tile.sprite;
+            Button button = newButton.GetComponent<Button>();
+            if (tile is RuleTile T)
+            {
+                buttonImage.sprite = T.m_DefaultSprite;
+            }
+            button.onClick.AddListener((() => 
+               _selectedTile = tile));
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Paint(InputAction.CallbackContext context)
     {
-        
+        if (_selectedTile == null) return;
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+        Vector3Int cellpos =  tilemap.WorldToCell(new Vector3(mousePosition.x, mousePosition.y, 0));
+        tilemap.SetTile(cellpos, _selectedTile);
     }
 }
