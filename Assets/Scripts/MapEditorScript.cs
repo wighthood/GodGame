4e141aad9a3bc64556bc0ba1de845ecbec5f78ad
wighthood <60099411+wighthood@users.noreply.github.com>
2025-10-03 14:15:14 +0200

    made painting click and drag

--- Assets/Scripts/MapEditorScript.cs
+++ Assets/Scripts/MapEditorScript.cs
@@ -1,6 +1,8 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.PlayerLoop;
 using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 
@@ -11,10 +13,13 @@ public class MapEditorScript : MonoBehaviour
     [SerializeField] private Transform selectionBar;
     [SerializeField] private GameObject buttonPrefab;
     
+    private Camera _camera;
     private TileBase _selectedTile;
+    private bool _isPainting = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _camera = Camera.main;
         foreach (TileBase tile in tiles)
         {
             GameObject newButton = Instantiate(buttonPrefab,selectionBar);
@@ -29,12 +34,30 @@ public class MapEditorScript : MonoBehaviour
         }
     }
 
+    public void DeselectTile(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _selectedTile = null;
+        }
+    }
+    
     public void Paint(InputAction.CallbackContext context)
     {
-        if (_selectedTile == null) return;
-        if (!context.performed) return;
-        if (Camera.main == null) return;
-        if (EventSystem.current.IsPointerOverGameObject()) return;
+        if (context.started)
+        {
+            _isPainting =  true;
+        }
+        else if (context.canceled)
+        {
+            _isPainting = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (_selectedTile == null|| _camera == null 
+                                 || EventSystem.current.IsPointerOverGameObject() || !_isPainting) return;
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         Vector3Int cellpos =  tilemap.WorldToCell(new Vector3(mousePosition.x, mousePosition.y, 0));
         tilemap.SetTile(cellpos, _selectedTile);
