cd1423776afccecbfe81a08a7d4b01237815f3ce
wighthood <60099411+wighthood@users.noreply.github.com>
2025-10-13 16:39:23 +0200

    made it so navmesh upadte on runtime when changing world

--- Assets/Scripts/MapEditorScript.cs
+++ Assets/Scripts/MapEditorScript.cs
@@ -1,15 +1,19 @@
+using System.Collections.Generic;
+using NavMeshPlus.Components;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
 using UnityEngine.UI;
+using NavMeshSurface = NavMeshPlus.Components.NavMeshSurface;
 
 public class MapEditorScript : MonoBehaviour
 {
     [SerializeField] private Tilemap tilemap;
-    [SerializeField] private TileBase[] tiles;
+    [SerializeField] private List<TileBase> tiles;
     [SerializeField] private Transform selectionBar;
     [SerializeField] private GameObject buttonPrefab;
+    [SerializeField] private NavMeshSurface navMesh;
     
     private Camera _camera;
     private TileBase _selectedTile;
@@ -55,10 +59,17 @@ public class MapEditorScript : MonoBehaviour
 
     private void Update()
     {
-        if (_selectedTile == null|| _camera == null 
-                                 || EventSystem.current.IsPointerOverGameObject() || !_isPainting) return;
+        if (_selectedTile == null || _camera == null
+                                  || EventSystem.current.IsPointerOverGameObject() || !_isPainting) return;
+        
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         Vector3Int cellpos =  tilemap.WorldToCell(new Vector3(mousePosition.x, mousePosition.y, 0));
+        TileBase TempTile = tilemap.GetTile(cellpos);
         tilemap.SetTile(cellpos, _selectedTile);
+        if (_selectedTile == tiles.Find(o => o.name == "WaterRule") && !TempTile.name.Contains("WaterRule")
+            || _selectedTile != tiles.Find(o => o.name == "WaterRule") && TempTile.name.Contains("WaterRule"))
+        {
+            navMesh.BuildNavMesh();
+        }
     }
 }
