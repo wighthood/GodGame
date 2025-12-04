9b43bc7fa7d2882244adf5d5cd32fb81864b98cd
AntoineCy <antoinecypiere351@gmail.com>
2025-12-04 17:31:08 +0100

    Update CamMap

--- Assets/Scripts/WorldGeneration.cs
+++ Assets/Scripts/WorldGeneration.cs
@@ -113,13 +113,13 @@ public class WorldGeneration : MonoBehaviour
         }
     }
 
-    public int MapWidth()
-    {
-        return mapWidth;
-    }
-
     public int MapHeight()
     {
         return mapHeight;
+    }
+
+    public int MapWidth()
+    {
+        return mapWidth;
     }
 }
