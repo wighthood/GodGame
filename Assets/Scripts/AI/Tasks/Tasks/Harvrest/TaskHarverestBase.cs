15a3b5012a24a86c023be8bdc63d6f32b4a7fbb1
SparkiiX987 <150683820+SparkiiX987@users.noreply.github.com>
2025-12-15 17:17:50 +0100

    fix construction task

--- Assets/Scripts/AI/Tasks/Tasks/Harvrest/TaskHarverestBase.cs
+++ Assets/Scripts/AI/Tasks/Tasks/Harvrest/TaskHarverestBase.cs
@@ -100,9 +100,7 @@ public abstract class TaskHarverestBase : TaskBase
 
     public override void DrawActionsGizmo()
     {
-        GUIStyle style = new GUIStyle();
-        style.normal.textColor = Color.green;
-        Handles.Label(manager.transform.position + Vector3.up * 0.5f + Vector3.left, $"doing Eat task", style);
+        base.DrawActionsGizmo();
 
         if (pathDebug == null || pathDebug.Count == 0)
             return;
